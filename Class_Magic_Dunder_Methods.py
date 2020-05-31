e47b9b19181f6e08d95ec86ddf8c2bcf33a0f8d7
N-avi-N <navin_k212@yahoo.com>
2020-05-31 17:15:42 -0500

    First Commit

---
+++ Class_Magic_Dunder_Methods.py
@@ -0,0 +1,39 @@
+class Employee:
+
+    raise_amt = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@comp.com'
+
+    def fullname(self):
+        return '{}, {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+    def __repr__(self):  # meant to be unambiguous
+        return 'Employee({}, {}, {})'.format(self.first, self.last, self.pay)
+
+    def __str__(self):  # meant to be readable, for user friendly
+        return '{} - {}'.format(self.fullname(), self.email)
+
+    def __add__(self, other):  # define bew dunder for addition of class instances
+        return self.pay + other.pay
+
+    def __len__(self):
+        return len(self.fullname())
+
+emp_1 = Employee('abc', 'xyz', 50000)
+emp_2 = Employee('def', 'ust', 60000)
+
+print(repr(emp_1))
+print(str(emp_1))
+
+print(emp_1 + emp_2)
+print(len(emp_1))
+
+print(1 + 2)
+print(int.__add__(1, 2))
